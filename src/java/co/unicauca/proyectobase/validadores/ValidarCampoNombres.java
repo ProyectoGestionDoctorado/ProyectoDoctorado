06c88ab8c03637c5dd130f369db175110209f5b7
zahydo <santiagodorado@unicauca.edu.co>
2017-03-21 08:47:25 -0500

    Se agregaron las clases para las validaciones de estudiantes

---
+++ src/java/co/unicauca/proyectobase/validadores/ValidarCampoNombres.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.unicauca.proyectobase.validadores;
+
+/**
+ *
+ * @author Sahydo
+ */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator(value="ValidarCampoNombres")
+public class ValidarCampoNombres implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+         String texto = String.valueOf(value);        
+        Pattern patron = Pattern.compile("([_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,}))");
+        Matcher encaja = patron.matcher(texto);        
+        if(!encaja.find())
+        {
+            FacesMessage msg= new FacesMessage(FacesMessage.SEVERITY_ERROR,"Formato de nombre inválido. Ej: Pepito","Formato de nombre inválido. Ej: Pepito");
+            throw new ValidatorException(msg);
+        }
+    }
+    
+}
